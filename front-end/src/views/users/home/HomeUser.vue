194f38eb59ce76547dda344c32d59d0c7b67b227
Ethan Ha <ethan.ha@avepoint.com>
2023-05-29 17:29:57 +0700

    Add route navbar

--- front-end/src/views/users/home/HomeUser.vue
+++ front-end/src/views/users/home/HomeUser.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import DocumentList from "../../../components/common/document-list/DocumentList.vue";
+import { RouteMap } from "../../../router/settingroute";
+const navBar = [
+  {
+    iconClass: "ms-Icon ms-Icon--CityNext",
+    title: "Tìm kiếm tài liệu",
+    pageName: RouteMap.search,
+  },
+];
 </script>
 <template>
   <div class="home-user">
